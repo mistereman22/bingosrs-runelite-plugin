40bb3500d0a3a064c44ba59ca014409359ca3fb0
mistereman22 <10224219+mistereman22@users.noreply.github.com>
2025-05-20 13:18:59 -0400

    Update defaults

--- src/test/java/com/bingosrs/BingOSRSPluginTest.java
+++ src/test/java/com/bingosrs/BingOSRSPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.bingosrs;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class BingOSRSPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(BingOSRSPlugin.class);
 		RuneLite.main(args);
 	}
 }
